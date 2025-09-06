f03dc5e1889aaee2cd289846766f5cef0da15ff6
Maria Fernanda <mariafernandasosa02@hotmail.com>
2025-09-06 04:27:08 -0300

    Agrega funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigoNuevo = document.getElementById("amigo").value;
+    console.log(amigoNuevo);
+    if (amigoNuevo.trim() == '') {
+        alert("Error: Debes ingresar un nombre.");
+    } else {
+        if (listaAmigos.includes(amigoNuevo)) {
+            alert("Error: Ese nombre ya fue ingresado.");
+        } else {
+            listaAmigos.push(amigoNuevo);
+            console.log(listaAmigos);
+            document.getElementById("amigo").value = "";
+            actualizarListaAmigos();
+        };
+    };
+};
